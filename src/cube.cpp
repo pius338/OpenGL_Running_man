c406b1ac498d736c786af077054195bc8f3d121d
Minsik Kim <ppp37686@gmail.com>
2024-10-16 02:13:57 +0900

    Update cube.cpp : CubeMan run now

--- src/cube.cpp
+++ src/cube.cpp
@@ -18,8 +18,9 @@ glm::mat4 viewMat;
 GLuint pvmMatrixID;
 
 float rotAngle = 0.0f;
-float speed = 5.0f;
-float max_angle = glm::radians(45.0f);
+float upDown = 0.0f;
+float speed = 8.0f;
+float max_angle = glm::radians(55.0f);
 int isDrawingCar = false;
 
 typedef glm::vec4  color4;
@@ -124,7 +125,7 @@ init()
 	pvmMatrixID = glGetUniformLocation(program, "mPVM");
 
 	projectMat = glm::perspective(glm::radians(10.0f), 1.0f, 0.1f, 100.0f);
-	viewMat = glm::lookAt(glm::vec3(0, 0, 10), glm::vec3(0, 0, 0), glm::vec3(0, 1, 0));
+	viewMat = glm::lookAt(glm::vec3(10, 0, 10), glm::vec3(0, 0, 0), glm::vec3(0, 1, 0));
 
 	glEnable(GL_DEPTH_TEST);
 	glClearColor(0.0, 0.0, 0.0, 1.0);
@@ -183,9 +184,9 @@ void drawMan(glm::mat4 manMat)
 	ArmSize[1] = glm::vec3(0.5, 1.9, 0.5); //ForeArm
 
 	LegPos[0] = glm::vec3(-0.55, -1.35, 0); //lUpperLeg
-	LegPos[1] = glm::vec3(-0.55, -2.8, 0); //lLowerLeg
+	LegPos[1] = glm::vec3(-0.55, -4.15, 0); //lLowerLeg
 	LegPos[2] = glm::vec3(0.55, -1.35, 0); //rUpperLeg
-	LegPos[3] = glm::vec3(0.55, -2.8, 0); //rLowerLeg
+	LegPos[3] = glm::vec3(0.55, -4.15, 0); //rLowerLeg
 
 	LegSize[0] = glm::vec3(0.8, 2.5, 0.8); //UpperLeg
 	LegSize[1] = glm::vec3(0.6, 2.5, 0.6); //LowerLeg
@@ -207,9 +208,16 @@ void drawMan(glm::mat4 manMat)
 	//Arm
 	for (int i = 0; i < 4; i++) 
 	{
+		float armAngle = (i < 2) ? rotAngle : -rotAngle;
 		modelMat = glm::translate(manMat, glm::vec3(0, 2.5, 0));
-		modelMat = glm::rotate(modelMat, rotAngle, glm::vec3(1, 0, 0));
+		modelMat = glm::rotate(modelMat, armAngle, glm::vec3(1, 0, 0));
 		modelMat = glm::translate(modelMat, glm::vec3(0, -2.5, 0));
+		if (i % 2)
+		{
+			modelMat = glm::translate(modelMat, glm::vec3(0, 0.4, 0));
+			modelMat = glm::rotate(modelMat, -abs(armAngle) * 2 , glm::vec3(1, 0, 0));
+			modelMat = glm::translate(modelMat, glm::vec3(0, -0.4, 0));
+		}
 		modelMat = glm::translate(modelMat, ArmPos[i]);
 		modelMat = glm::scale(modelMat, ArmSize[i%2]);
 		pvmMat = projectMat * viewMat * modelMat;
@@ -219,20 +227,24 @@ void drawMan(glm::mat4 manMat)
 
 	for (int i = 0; i < 4; i++)
 	{
-		modelMat = glm::translate(manMat, LegPos[i]);
+
+		float legAngle = (i < 2) ? -rotAngle * 1.5 : rotAngle * 1.5;
+		float lowerLegAngle = legAngle * 0.8;
+		modelMat = glm::translate(manMat, glm::vec3(0, -0.4, 0));
+		modelMat = glm::rotate(modelMat, legAngle, glm::vec3(1, 0, 0));
+		modelMat = glm::translate(modelMat, glm::vec3(0, 0.4, 0));
+		if (i % 2)
+		{
+			modelMat = glm::translate(modelMat, glm::vec3(0, -2.5, 0));
+			modelMat = glm::rotate(modelMat, abs(lowerLegAngle), glm::vec3(1, 0, 0));
+			modelMat = glm::translate(modelMat, glm::vec3(0, 2.5, 0));
+		}
+		modelMat = glm::translate(modelMat, LegPos[i]);
 		modelMat = glm::scale(modelMat, LegSize[i % 2]);
 		pvmMat = projectMat * viewMat * modelMat;
 		glUniformMatrix4fv(pvmMatrixID, 1, GL_FALSE, &pvmMat[0][0]);
 		glDrawArrays(GL_TRIANGLES, 0, NumVertices);
 	}
-
-	//lForeArm
-	//rArm
-	//rForeArm
-	//lUpperLeg
-	//lLowerLeg
-	//rUpperLeg
-	//rLowerLeg
 }
 
 //----------------------------------------------------------------------------
@@ -242,13 +254,13 @@ void display(void)
 	glm::mat4 worldMat, pvmMat;
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-	worldMat = glm::rotate(glm::mat4(1.0f), rotAngle, glm::vec3(1.0f, 1.0f, 0.0f));
+	//worldMat = glm::rotate(glm::mat4(1.0f), rotAngle, glm::vec3(1.0f, 1.0f, 0.0f));
+	worldMat = glm::translate(glm::mat4(1.0f), glm::vec3(0,upDown,0));
+	worldMat = glm::rotate(worldMat, glm::radians(8.0f), glm::vec3(1, 0, 0));
 
 	if (isDrawingCar)
 	{
-		//drawCar(worldMat);
-		drawMan(glm::mat4(1.0f));
-		//drawMan(worldMat);
+		drawMan(worldMat);
 	}
 	else
 	{
@@ -268,9 +280,10 @@ void idle()
 	int currTime = glutGet(GLUT_ELAPSED_TIME);
 
 	float t = abs(currTime - prevTime)/1000.0f;
-	//rotAngle += glm::radians(t*360.0f / 10000.0f);
+
 	rotAngle = max_angle * std::sin(t * speed);
-	std::cout << rotAngle << '\n';
+	
+	upDown = (max_angle / 3) * std::sin(t * speed * 2.0f);
 
 	glutPostRedisplay();
 }
